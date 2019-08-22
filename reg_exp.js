2be7826abbc8dc3e19c4dc8f82154170db6ae010
KMR-repo <kmrchr051@gmail.com>
2019-08-23 01:03:31 +0530

    GH-Pages

---
+++ reg_exp.js
@@ -0,0 +1,37 @@
+function Reg_Exp()
+{
+    var sRegExp = document.getElementById("Input").value
+    //var sRegExp = "Test";
+    var test = 'ABC';
+    //console.log(test[0]);
+    var sMatch = /^[A-Za-z0-9.?]*$/;
+    var test = ("." + sRegExp.match(sMatch)).match(/^[.?]*$|^\.null$/);
+    console.log(test);
+    if(test != null)
+    {
+        alert("Error in regular Expression");
+    }
+    /*
+    var Test1 = test.match(/^null|[.?]*$/);
+    console.log(test);
+    console.log(Test1);
+    */
+    /*
+    if(test != null)
+    {
+        if(test.length != 0)
+        {
+            for(var i = 0; i <test.length; i++)
+            {
+                console.log(test[i]);
+            }
+        }
+        else
+        console.log("null");
+    }
+    else
+    {
+        console.log("null");
+    }
+    */
+}
